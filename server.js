4e35f3fe729c5a06ddd7e4d2e58537231509001b
Alissa Cohan <alissacohan18@gmail.com>
2023-01-21 15:10:45 -0500

    add Heroku

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const { animals } = require("./data/animals");
+
+const app = express();
+
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    //treating personality traits different since it can be an array vs a string (single value)
+    if (query.personalityTraits) {
+          // If personalityTraits is a string, place it into a new array and save.
+      if (typeof query.personalityTraits === 'string') {
+        personalityTraitsArray = [query.personalityTraits];
+      } else {
+        personalityTraitsArray = query.personalityTraits;
+      }
+      // Loop through each trait in the personalityTraits array:
+      personalityTraitsArray.forEach(trait => {
+        // At the end we'll have an array of animals that have every one of the traits
+        filteredResults = filteredResults.filter(
+          animal => animal.personalityTraits.indexOf(trait) !== -1
+        );
+      });
+    }
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    // return the filtered results:
+    return filteredResults;
+  }
+
+  app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
+
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+});
+
